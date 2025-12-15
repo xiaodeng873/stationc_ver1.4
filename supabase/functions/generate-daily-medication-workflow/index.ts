de0798eaa3e3b3a1075a5d4f022d7fd140125401
xiaodeng873 <xiaodeng873@gmail.com>
2025-12-15 10:15:13 +0800

    Updated index.ts

--- supabase/functions/generate-daily-medication-workflow/index.ts
+++ supabase/functions/generate-daily-medication-workflow/index.ts
@@ -3,7 +3,7 @@ import { createClient } from 'npm:@supabase/supabase-js@2';
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Client-Info, Apikey",
 };
 
 interface Prescription {
